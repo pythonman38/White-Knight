0c89b042cf7b6d39ceffeb45cc1a348460bbba04
Michael Walker <pythonk@email.phoenix.edu>
2025-11-13 16:02:59 -0500

    Section 2: Movement and Camera Controls
    Added a spring arm node to the player for smoother camera transitions during gameplay. Also added some interpolation calculations to simulate relative movement (the player moves in the direction it's facing, even after rotation). Finally, just for a better scene to work with, I added some vegetation and ground color to make it a bit more visually pleasing during game development. Most of these functions are built into the editor, so I didn't need to write much code to accomplish this. Next commit will focus on Player animation.

--- Scripts/Characters/Player/player.gd
+++ Scripts/Characters/Player/player.gd
@@ -2,12 +2,24 @@ extends CharacterBody3D
 class_name Player
 
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+
+var _look := Vector2.ZERO
+
+@export var mouse_sensitivity: float = 0.00375
+@export var min_boundary: float = -60.0
+@export var max_boundary: float = 10.0
+
+@onready var horizontal_pivot: Node3D = $HorizontalPivot
+@onready var vertical_pivot: Node3D = $HorizontalPivot/VerticalPivot
+
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta: float) -> void:
+	frame_camera_rotation()
 	if not is_on_floor(): velocity += get_gravity() * delta
-	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction := get_movement_direction()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -15,3 +27,19 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 	move_and_slide()
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"): Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		if event is InputEventMouseMotion: _look += -event.relative * mouse_sensitivity
+
+func get_movement_direction() -> Vector3:
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var input_vector := Vector3(input_dir.x, 0, input_dir.y).normalized()
+	return horizontal_pivot.global_transform.basis * input_vector
+
+func frame_camera_rotation() -> void:
+	horizontal_pivot.rotate_y(_look.x)
+	vertical_pivot.rotate_x(_look.y)
+	vertical_pivot.rotation.x = clampf(vertical_pivot.rotation.x, deg_to_rad(min_boundary), deg_to_rad(max_boundary))
+	_look = Vector2.ZERO
