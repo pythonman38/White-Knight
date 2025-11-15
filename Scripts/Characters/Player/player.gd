6a263eef48487dbcb316badb8cbb68b08b4eb039
Michael Walker <pythonk@email.phoenix.edu>
2025-11-15 10:10:18 -0500

    Section 3: Animating the Player
    To start, I created a character rig scene and added it to the Player with one of the Knight meshes exposed. I then added a simple animation tree to control the blending of character movement and attacking (slashing). Added a bone attachment to place the sword and shield in the correct hands of the player. Finally, I added a ray cast node to the Player's hand holding the sword (right hand) to toggle collision while swinging the sword. Now we need something to attack. Next commit will focus on creating basic enemies that will deal damage to and receive damage from the Player.

--- Scripts/Characters/Player/player.gd
+++ Scripts/Characters/Player/player.gd
@@ -4,13 +4,19 @@ class_name Player
 const SPEED = 5.0
 
 var _look := Vector2.ZERO
+var _attack_direction := Vector3.ZERO
 
 @export var mouse_sensitivity: float = 0.00375
 @export var min_boundary: float = -60.0
 @export var max_boundary: float = 10.0
+@export var animation_decay: float = 20.00
+@export var attack_move_speed: float = 3.0
 
 @onready var horizontal_pivot: Node3D = $HorizontalPivot
 @onready var vertical_pivot: Node3D = $HorizontalPivot/VerticalPivot
+@onready var rig_pivot: Node3D = $RigPivot
+@onready var rig: Node3D = $RigPivot/Rig
+@onready var attack_cast: RayCast3D = %AttackCast
 
 
 func _ready() -> void:
@@ -18,20 +24,19 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	frame_camera_rotation()
-	if not is_on_floor(): velocity += get_gravity() * delta
 	var direction := get_movement_direction()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+	rig.update_animation_tree(direction)
+	handle_idle_physics_frame(delta, direction)
+	handle_slashing_physics_frame(delta)
+	if not is_on_floor(): velocity += get_gravity() * delta
 	move_and_slide()
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"): Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		if event is InputEventMouseMotion: _look += -event.relative * mouse_sensitivity
+	if rig.is_idle():
+		if event.is_action_pressed("click"): slash_attack()
 
 func get_movement_direction() -> Vector3:
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
@@ -43,3 +48,30 @@ func frame_camera_rotation() -> void:
 	vertical_pivot.rotate_x(_look.y)
 	vertical_pivot.rotation.x = clampf(vertical_pivot.rotation.x, deg_to_rad(min_boundary), deg_to_rad(max_boundary))
 	_look = Vector2.ZERO
+
+func look_toward_direction(direction: Vector3, delta: float) -> void:
+	var target_transform := rig_pivot.global_transform.looking_at(rig_pivot.global_position + direction, Vector3.UP, true)
+	rig_pivot.global_transform = rig_pivot.global_transform.interpolate_with(target_transform, 1.0 - exp(-animation_decay * delta))
+
+func slash_attack() -> void:
+	rig.travel("Slash")
+	_attack_direction = get_movement_direction()
+	if _attack_direction.is_zero_approx(): _attack_direction = rig.global_basis * Vector3(0, 0, 1)
+	attack_cast.clear_exceptions()
+
+func handle_idle_physics_frame(delta: float, direction: Vector3) -> void:
+	if not rig.is_idle: return
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+		look_toward_direction(direction, delta)
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+func handle_slashing_physics_frame(delta: float) -> void:
+	if not rig.is_slashing(): return
+	velocity.x = _attack_direction.x * attack_move_speed
+	velocity.z = _attack_direction.z * attack_move_speed
+	look_toward_direction(_attack_direction, delta)
+	attack_cast.deal_damage()
